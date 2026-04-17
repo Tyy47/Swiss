1a9413d2d936dddea56d04e36bbcf56c9cb433bf
Tyy47 <Tylerwmack321@gmail.com>
2026-04-17 18:05:50 -0400

    update: changed how sc push worked, message is now optional.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,18 +1,25 @@
 # Swiss 1.1.0
 
-## Init
+## New Modules:
+
+### Shortcuts:
+#### The new shortcut module for Swiss provides a set of commands that combine multiple commands into one to save typing time.
+- Commit - The commit shortcut combines git add . & git commit -m "commit message here".
+- Push - The push shortcut combines git add . & git commit -m "" & pushes your commits to the added repository.
+
+### Init
 - Added support for zig
 - Added support for python using uv
 - Added jujutsu vc support for starting a project
 - Reworked web init. Now able to specify the framework when building the web project.
 
-## Build
+### Build
 - Added support for zig
 
-## Run
+### Run
 - Added support for python
 
-## Misc
+### Misc
 - Rebuilt arg parsing system to work on a registry storage system. This now allows for more flexible commands and better growth structurally.
 - Net module documentation added to DOCS.md
 - Added jujutsu startup to DOCS.md
--- shortcuts/sc.go
+++ shortcuts/sc.go
@@ -3,6 +3,7 @@ package shortcuts
 import "swiss/utils"
 
 func GitCommitSC() {
+	// Gathers a commit message
 	var commitMessage string
 	if len(utils.AdditionalArguments) <= 0 {
 		utils.Warning("Commit message is blank, fill in commit message to continue.")
@@ -10,25 +11,29 @@ func GitCommitSC() {
 	} else {
 		commitMessage = utils.AdditionalArguments[0]
 	}
-
+	
+	// Adds all files to the commit with a message.
 	utils.RunCommand("git", "add", ".")
 	utils.RunCommand("git", "commit", "-m", commitMessage)
 	
+	// Message stating that the commit was created.
 	utils.Success("Commit created.")
 }
 
 func GitPushSC() {
+	// Adds all changed files to commit
+	utils.RunCommand("git", "add", ".")
+
+	// Gathers a commit message if one is available.
 	var commitMessage string
-	if len(utils.AdditionalArguments) <= 0 {
-		utils.Warning("Commit message is blank, fill in commit message to continue.")
-		return
-	} else {
+	if len(utils.AdditionalArguments) > 0 {
 		commitMessage = utils.AdditionalArguments[0]
+		utils.RunCommand("git", "commit", "-m", commitMessage)
 	}
 
-	utils.RunCommand("git", "add", ".")
-	utils.RunCommand("git", "commit", "-m", commitMessage)
+	// Pushes changes to repository
 	utils.RunCommand("git", "push")
 
+	// Message stating that changes were pushed
 	utils.Success("Commit pushed to repository.")
 }
