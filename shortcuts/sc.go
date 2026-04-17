e9e0898536a76b0c0eba66b43848e2b92b74db47
Tyy47 <Tylerwmack321@gmail.com>
2026-04-17 17:42:02 -0400

    update: changed how runcommand function works, removed success message at the end to get rid of muted argument.

--- shortcuts/sc.go
+++ shortcuts/sc.go
@@ -11,22 +11,24 @@ func GitCommitSC() {
 		commitMessage = utils.AdditionalArguments[0]
 	}
 
-	utils.RunCommand(true, "git", "add", ".")
-	utils.RunCommand(true, "git", "commit", "-m", commitMessage)
+	utils.RunCommand("git", "add", ".")
+	utils.RunCommand("git", "commit", "-m", commitMessage)
 	
 	utils.Success("Commit created.")
 }
 
 func GitPushSC() {
-	commitMessage := utils.CheckArguments(utils.Arguments, 4, 3)
-	if commitMessage == "" {
+	var commitMessage string
+	if len(utils.AdditionalArguments) <= 0 {
 		utils.Warning("Commit message is blank, fill in commit message to continue.")
 		return
+	} else {
+		commitMessage = utils.AdditionalArguments[0]
 	}
 
-	utils.RunCommand(true, "git", "add", ".")
-	utils.RunCommand(true, "git", "commit", "-m", commitMessage)
-	utils.RunCommand(true, "git", "push")
+	utils.RunCommand("git", "add", ".")
+	utils.RunCommand("git", "commit", "-m", commitMessage)
+	utils.RunCommand("git", "push")
 
 	utils.Success("Commit pushed to repository.")
 }
