0e437a224dd0805ba5941f6c4582a43b6b07a2ee
Tyy47 <tylerwmack321@gmail.com>
2026-04-25 19:10:20 -0400

    docs: finished writing networking module function documentation

--- network/network.go
+++ network/network.go
@@ -100,30 +100,42 @@ func GetAddresses() {
 	}
 }
 
+// Connects to an address and writes collected addresses to a network output file.
 func writeAddresses() {
+	// Connects to the endpoint
 	conn, err := net.LookupIP(endpoint)
+	// Checks if the function crashes
 	networkCrashError(err, "IP Addresses")
+	// Open a new writer to write address output.
 	write := bufio.NewWriter(outputFile.file)
-
+	
+	// Writes IP Addresses title in output file and crashes if unable to write
 	if _, err := write.WriteString("\nIP Addresses: \n"); err != nil {
 		utils.CrashCheck(err)
 	}
-
+	
+	// Collects all available IP addresses and converts them to strings.
 	for rows := range conn {
 		val := conn[rows].String()
-
+	
+		// Writes all available addresses to output file and crashes if unable to write.
 		if _, err := write.WriteString(val + "\n"); err != nil {
 			utils.CrashCheck(err)
 		}
 	}
-
+	
+	// Closes the writer once the function is complete
 	write.Flush()
 }
 
+// Collects nameservers from a given address and prints it to the console.
 func GetNameServer() {
+	// Connects to the endpoint to retrieve nameservers.
 	conn, err := net.LookupNS(endpoint)
+	// Crashes the program if unable to connect
 	networkCrashError(err, "Name servers")
-
+	
+	// Prints all available nameservers to the console.
 	utils.Success("\nNameservers for " + endpoint + ".")
 	for rows := range conn {
 		val := conn[rows].Host + "\n"
@@ -131,11 +143,16 @@ func GetNameServer() {
 	}
 }
 
+// Collects namesevers from a given address and writes them to a network output file.
 func writeNameServer() {
+	// Connects to the endpoint to retrieve nameservers.
 	conn, err := net.LookupNS(endpoint)
+	// Crashes the program if unable to connect.
 	networkCrashError(err, "Name servers")
 
+	// Opens up the file in a new "writer"
 	write := bufio.NewWriter(outputFile.file)
+	// Writes all nameservers to output file, crashes the program if unable to write.
 	if _, err := write.WriteString("\nName Servers: \n"); err != nil {
 		utils.CrashCheck(err)
 	}
@@ -147,91 +164,126 @@ func writeNameServer() {
 			utils.Crash(err)
 		}
 	}
-
+	
+	// Closes the writer once the function is complete
 	write.Flush()
 }
 
+// Connects to a given address and retrieves known CNAME records.
 func GetCNameRecords() {
+	// Connects to the endpoint to retreive the records
 	conn, err := net.LookupCNAME(endpoint)
+	// Crashes if unable to retrieve records
 	networkCrashError(err, "CNAME records")
-
+	
+	// Prints the known CNAME records to the console.
 	utils.Success("CNAME Records for " + endpoint + ".")
 	fmt.Println(conn)
 }
 
+// Connects to a given address, retrieves known CNAME records, and writes them to a network output file.
 func writeCNameRecords() {
+	// Connects to the endpoint to get the CNAME records
 	conn, err := net.LookupCNAME(endpoint)
+	// Crashes the program if unable to connect
 	networkCrashError(err, "CNAME records")
-
+	
+	// Opens up a writer to write in the file
 	write := bufio.NewWriter(outputFile.file)
+	// Writes the CNAME records title in file.
 	if _, err := write.WriteString("\nCNAME Records: \n"); err != nil {
 		utils.CrashCheck(err)
 	}
-
+	
+	// Writes CNAME records into network output file.
 	_, errr := write.WriteString(conn)
 	if errr != nil {
 		utils.CrashCheck(err)
 	}
-
+	
+	// Closes the file once function is complete
 	write.Flush()
 }
 
+// Connects to an endpoint, retrieves TXT Records, and prints them to the console.
 func GetTXTRecords() {
+	// Connects to the endpoint to retrieve TXT records
 	conn, err := net.LookupTXT(endpoint)
+	// Crashes the program if unable to connect
 	networkCrashError(err, "TXT records")
-
+	
+	// Prints all known TXT records to the console.
 	utils.Success("TXT Records for " + endpoint + ".")
 	for rows := range conn {
 		fmt.Println(conn[rows])
 	}
 }
 
+// Connects to a given endpoint, retrieves TXT records, and writes them to a network output file.
 func writeTXTRecords() {
+	// Connects to the endpoint to retrieve the records
 	conn, err := net.LookupTXT(endpoint)
+	// Crashes if unable to connect
 	networkCrashError(err, "TXT records")
-
+	
+	// Opens a new writer to write output file
 	write := bufio.NewWriter(outputFile.file)
+	// Writes TXT records title in output file
 	if _, err := write.WriteString("\n\nTXT Records: \n"); err != nil {
 		utils.CrashCheck(err)
 	}
-
+	
+	// Writes all TXT records to network output file
 	for rows := range conn {
 		_, err := write.WriteString(conn[rows] + "\n")
 		utils.CrashCheck(err)
 	}
-
+	
+	// Closes the writer when the function is complete
 	write.Flush()
 }
 
+// Connects to a given endpoint, collects known MX records, and prints them to the console.
 func GetMXRecords() {
+	// Connects to the endpoint to retrieve MX records
 	conn, err := net.LookupMX(endpoint)
+	// Crashes the program if unable to connect.
 	networkCrashError(err, "MX records")
-
+	
+	// Success message for printing to the console
 	utils.Success("MX Records for " + endpoint + ".")
-
+	
+	// Prints all known MX Records to the console.
 	for rows := range conn {
 		fmt.Println("Host: " + conn[rows].Host + "\n")
 	}
 }
 
+// Connects to a given domain, collects MX records, and writes them to the network output file.
 func writeMXRecords() {
+	// Connects to the given domain to retrieve MX records.
 	conn, err := net.LookupMX(endpoint)
+	// Crashes the program if unable to connect.
 	networkCrashError(err, "MX records")
-
+	
+	// Opens a new writer to write records to output file
 	write := bufio.NewWriter(outputFile.file)
+	// Writes MX records title in output file
 	if _, err := write.WriteString("\nMX Records: \n"); err != nil {
 		utils.CrashCheck(err)
 	}
-
+	
+	// Writes each MX records into output file.
 	for rows := range conn {
 		_, err := write.WriteString("Host: " + conn[rows].Host + "\n")
 		utils.CrashCheck(err)
 	}
 
+	// Closes the writer when the function is complete
 	write.Flush()
 }
 
-// Creates the output file for net
+// Creates the network output file where all information is stored when swiss net gather is ran.
 func initFileCreation() {
 	if !utils.CheckFileExists(outputFile.name) {
 		utils.MakeFile(outputFile.name, true)
