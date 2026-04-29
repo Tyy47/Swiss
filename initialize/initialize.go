7ffa427d099621e60e5a73f4248957bf67e005b4
Tyy47 <tylerwmack321@gmail.com>
2026-04-29 12:28:08 -0400

    docs & update: added documentation to initialize module and added crash reasons to init functions (jj & git).

--- initialize/initialize.go
+++ initialize/initialize.go
@@ -2,7 +2,6 @@ package initialize
 
 import (
 	"fmt"
-	"log"
 	"os"
 	"os/exec"
 	"strings"
@@ -117,51 +116,66 @@ func flagHandler(additonalArgs *[]string, proj project) {
 
 // Inits git in current directory when called.
 func gitInit(proj project) error {
+	// Changes directory into init'd project folder
 	if proj.Name != "" {
 		os.Chdir("./" + proj.Name)
-		defer os.Chdir("..")
+		defer os.Chdir("..") // Changes back to original directory when function finishes
 	} else {
-		// Make gitignore before adding files to repo so it can get added.
+		// Creates all the files that are ususally in a repository
 		utils.MakeFile(".gitignore", false)
 		utils.MakeFile("TODO.md", false)
 		utils.MakeFile("README.md", false)
 	}
-
+	
+	// Initing git command
 	init := exec.Command("git", "init")
-
+	
+	// Runs git init and returns an error if unable to 
 	if err := init.Run(); err != nil {
 		utils.Error("Unable to init git")
+		utils.Reason(err.Error())
 		return err
 	}
-
+	
+	// Command to add all files to the repository
 	add := exec.Command("git", "add", ".")
-
+	
+	// Runs the add command and returns the error if unsuccessful
 	if err := add.Run(); err != nil {
 		utils.Error("Unable to add files to git project")
+		utils.Reason(err.Error())
 		return err
 	}
-
+	
+	// Renames the master branch to "main" command
 	mainBranch := exec.Command("git", "branch", "-M", "main")
-
+	
+	// Runs the rename command and returns the err if unsuccessful
 	if err := mainBranch.Run(); err != nil {
 		utils.Error("Unable to change main branch to 'main'.")
+		utils.Reason(err.Error())
 		return err
 	}
-
+	
+	// Success message when finished
 	utils.Success("Git has been initialized.")
-
+	
 	return nil
 }
 
 // Inits jujutsu in current directory when called.
 func jjInit() error {
+	// Init jujutsu command
 	init := exec.Command("jj", "git", "init")
-
+	
+	// Runs the init command and returns the error if unsuccessful
 	if err := init.Run(); err != nil {
 		utils.Error("Unable to init JJ")
+		utils.Reason(err.Error())
 		return err
 	}
-
+	
+	// Success message if jj has been init'd
 	utils.Success("Jujutsu has been initialized.")
 
 	return nil
@@ -247,6 +261,7 @@ func createPythonProject() project {
 	return program
 }
 
+// Creates a web based project. Can be modified by users arguments for differing frameworks
 func getWebProject() project {
 	programName := utils.GetUserInput("Enter your project name: ", "my-app")
 
@@ -289,17 +304,23 @@ func CreateWebProject() {
 }
 
 func CreateProject() {
+	// Length check for later code
 	if len(utils.Arguments) < 3 {
 		return
 	}
-
+	
+	// Grabs the name of the project the user init's
 	argument := strings.ToLower(utils.Arguments[2])
+	// Loops through projects in the registry
 	for project := range len(registry.projects) {
+		// Checks if an argument is in the products registry
 		if argument == registry.projects[project].Language {
+			// Runs the initalize method of the project if the project has been found, if it's an error, it checks if it's a crash.
 			if err := registry.projects[project].initialize(); err != nil {
-				log.Fatal(err)
+				utils.CrashCheck(err)
 				return
 			} else {
+				// Executes all the additional flags if any are provided by the user.
 				flagHandler(&utils.AdditionalArguments, registry.projects[project])
 				utils.Success(registry.projects[project].Language + " project has been created.")
 				return
@@ -307,6 +328,7 @@ func CreateProject() {
 			}
 		}
 	}
+	// Message to the user if a language is not found in the projects registry list.
 	utils.Error("Unable to find " + argument + " in registry list.")
 }
 
